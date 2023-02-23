e4fa667661162b9d87cf39efed9ae2d81158bee8
fkohl04 <52261165+fkohl04@users.noreply.github.com>
2023-02-23 18:55:28 +0100

    add dependabot config for test services (#15)
    Co-authored-by: fkohl04 <fkohl04@googlemail.com>

--- example-applications/ktor-example/build.gradle.kts
+++ example-applications/ktor-example/build.gradle.kts
@@ -1,15 +1,15 @@
-val ktor_version="2.2.1"
-val kotlin_version="1.7.22"
-val prometeus_version="1.6.3"
-val logback_version="1.4.5"
+val ktorVersion="2.2.1"
+val kotlinVersion="1.7.22"
+val prometheusVersion="1.10.4"
+val logbackVersion="1.4.5"
 
 plugins {
-    kotlin("jvm") version "1.7.22"
+    kotlin("jvm") version "1.8.10"
     id("io.ktor.plugin") version "2.2.1"
 }
 
 group = "fkohl04.expiration.monitoring"
-version = "1.0-SNAPSHOT"
+version = "0.0.1-SNAPSHOT"
 
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
@@ -21,11 +21,11 @@ repositories {
 
 dependencies {
     implementation(project(":expiration-monitoring-core"))
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-metrics-micrometer-jvm:$ktor_version")
-    implementation("io.micrometer:micrometer-registry-prometheus:$prometeus_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("io.ktor:ktor-server-metrics-micrometer-jvm:$ktorVersion")
+    implementation("io.micrometer:micrometer-registry-prometheus:$prometheusVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 }
